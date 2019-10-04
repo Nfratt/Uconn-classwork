b063c2ec8a75cdddfeed911caa6e1cdbca2ab6f5
Nick Frattaroli <Nafratt@gmail.com>
2019-10-03 20:13:56 -0400

    adding during break

--- Node/arguments.js
+++ Node/arguments.js
@@ -1,4 +1,10 @@
 // process.argv will print out any command line arguments.
-console.log(process.argv);
-console.log(process.argv.length);
-// console.log(process.env);
+// console.log(process.argv);
+// console.log(process.argv.length);
+// // console.log(process.env);
+// // Adds two command line arguments (numbers).
+// console.log(parseFloat(process.argv[2]) + parseFloat(process.argv[3]));
+
+const num1 =process.argv[2],
+num2 = process.argv[3];
+console.log(num1===num2);
