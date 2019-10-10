e22e8f0bdc5364ce89e1a52d40569145d576367f
Nick Frattaroli <Nafratt@gmail.com>
2019-10-10 19:25:38 -0400

    pushing new changes

--- Hard-Hat-Zone-Constructors-at-Work/01-RainingCatsAndDogs-NoCon/index.js
+++ Hard-Hat-Zone-Constructors-at-Work/01-RainingCatsAndDogs-NoCon/index.js
@@ -1,59 +1,28 @@
-// creates an object and sets it to "dogs"
-const dogs = {
-  // creates the property "raining" and sets it to true
+// DRY=do not repeat yourself
+// this is how you set up a object
+const dog={
   raining: true,
-  // creates the property "noise" and sets it to "Woof!"
-  noise: 'Woof!',
-
-  makeNoise: function() {
-    if (this.raining === true) {
-      console.log(this.noise);
-    }
+  noise: 'woof!',
+  makenoise: function() {
+    if (this.raining) console.log(this.noise);
   },
-
-  // creates the method "makeNoise", which when called,
-  // prints dogs.noise if .raining is true
-  makeNoiseBroken: () => {
-  // the 'this' keyword DOES NOT refer to the object it's called from
-  // i.e. this.raining does not exist
-    if (this.raining === true) {
-      console.log(this.noise);
-    }
-  },
-
-  makeNoiseNew() {
-    // the 'this' keyword here still refers to the object it's called from
-    // i.e. this.raining refers to the raining property
-    // of 'this' particular object
-    if (this.raining === true) {
-      console.log(this.noise);
-    }
-  },
-
 };
-
-// creates an object with a similar layout to "dogs" and sets it to "cats"
-const cats = {
+const cat={
   raining: false,
-  noise: 'Meow!',
-  makeNoise: function() {
-    if (this.raining === true) {
+  noise: 'meow!',
+  makenoise: function() {
+    if (this.raining) {
       console.log(this.noise);
     }
   },
 };
+cat.raining=true;
+dog.makenoise();
+cat.makenoise();
 
-// calls the "makeNoise" methods for both objects
-dogs.makeNoiseNew();
-cats.raining = true;
-cats.makeNoise();
 
-// creates a function called "massHysteria" which takes in both objects and
-// prints a message to the screen if ".raining" is true for both of them
-const massHysteria = function(dogs, cats) {
-  if (dogs.raining === true && cats.raining === true) {
-    console.log('DOGS AND CATS LIVING TOGETHER! MASS HYSTERIA!');
+function massHysteria(animal1, animal2) {
+  if (animal1.raining && animal2.raining) {
+    console.log('"DOGS AND CATS LIVING TOGETHER! MASS HYSTERIA!');
   }
-};
-
-massHysteria(dogs, cats);
+}
--- Hard-Hat-Zone-Constructors-at-Work/02-RainingCatsAndDogs-Con/index.js
+++ Hard-Hat-Zone-Constructors-at-Work/02-RainingCatsAndDogs-Con/index.js
@@ -5,10 +5,12 @@
  * @param {*} raining
  * @param {*} noise
  */
+// constructor function
+// eslint-disable-next-line require-jsdoc
 function Animal(raining, noise) {
   this.raining = raining;
   this.noise = noise;
-  this.makeNoise = function() {
+  this.makeNoise =() => {
     if (this.raining === true) {
       console.log(this.noise);
     }
@@ -17,8 +19,13 @@ function Animal(raining, noise) {
 
 // sets the variables "dogs" and "cats" to be animal objects
 // and initializes them with raining and noise properties
+// new adds a attribute to our constcutor function
 const dogs = new Animal(true, 'Woof!');
 const cats = new Animal(false, 'Meow!');
+const allmycats=[];
+for (let i =0; i<39; i++) {
+  allmycats.push(new Animal(true, 'meow!'));
+}
 
 // calling dogs and cats makeNoise methods
 dogs.makeNoise();
@@ -27,6 +34,7 @@ cats.makeNoise();
 // if we want, we can change an objects properties after they're created
 cats.raining = true;
 cats.makeNoise();
+allmycats[10].makeNoise();
 
 const massHysteria = function(dogs, cats) {
   if (dogs.raining === true && cats.raining === true) {
