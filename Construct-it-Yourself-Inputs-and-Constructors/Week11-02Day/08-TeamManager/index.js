aa35df49eba5287a14506a4bc41b638f7271f64b
Nick Frattaroli <Nafratt@gmail.com>
2019-10-12 11:49:58 -0400

    adding recursion

--- Construct-it-Yourself-Inputs-and-Constructors/Week11-02Day/08-TeamManager/index.js
+++ Construct-it-Yourself-Inputs-and-Constructors/Week11-02Day/08-TeamManager/index.js
@@ -1,44 +1,59 @@
+/* eslint-disable valid-jsdoc */
 // dependency for inquirer npm package
 const inquirer = require('inquirer');
 
 /**
  * A programmer person
  */
-class Programmer {
+class Player {
   /**
    *
    * @param {string} name Person's name
    * @param {string} position As assigned by HR
-   * @param {number} age 0 - 99
-   * @param {string} language just their primary language
+   // eslint-disable-next-line valid-jsdoc
+   * @param {number} offense 0 -10
+   * @param {number} defense 0-10
    */
-  constructor(name, position, age, language) {
+  constructor(name, position, offense, defense) {
     this.name = name;
     this.position = position;
-    this.age = age;
-    this.language = language;
+    this.offense = offense;
+    this.defense = defense;
   }
 
   /**
    * Prints out all properties
    */
-  printInfo() {
+  printStats() {
     console.log(`
       Name: ${this.name}
       Position: ${this.position}
-      Age: ${this.age}
-      Language: ${this.language}
+      offense: ${this.offense}
+      defense: ${this.defense}
 
     `);
+    // const starters=[];
+    // const subs=[];
+    //     goodgame() {
+    //         Player.offence=++,
+    //         Player.defense=++
+
+    //     }
+    // badgame(){
+    //     Player.offence=--,
+    //     player.defense=--,
+    // }
   }
 }
 // variable we will use to count how many times our questions have been asked
-// let count = 0;
+let count = 0;
+const coders= [];
 
-const askQuestion = function(isDone) {
+const addPlayer = function() {
   // if statement to ensure that our questions are only asked five times
-  if (isDone) {
-    console.log('ok, breaktime');
+  if (count >= 3) {
+    coders.forEach((it) => it.printStats());
+    console.log('is done');
     return;
   }
 
@@ -47,40 +62,46 @@ const askQuestion = function(isDone) {
   inquirer.prompt([
     {
       name: 'name',
-      message: 'What is your name?',
+      message: 'What is the players name?',
     }, {
       name: 'position',
       message: 'What is your current position?',
     }, {
-      name: 'age',
-      message: 'How old are you?',
+      name: 'offense',
+      message: 'whats  their offense score',
     }, {
-      name: 'language',
-      message: 'What is your favorite programming language?',
-    }, {
-      name: 'isDone',
-      message: 'are u done?',
+      name: 'defense',
+      message: 'Whats their defense score?',
     },
   ]).then((answers) => {
     // initializes the variable newProgrammer
     // to be a programmer object which will take
     // in all of the user's answers to the questions above
-    const newProgrammer = new Programmer(
+    const roster = new Player(
         answers.name,
         answers.position,
-        answers.age,
-        answers.language
+        answers.offense,
+        answers.defense
     );
-      // printInfo method is run to show that the
-      // newProgrammer object was successfully created and filled
-    newProgrammer.printInfo();
+
+    // add the new Programmer to the answers array
+    coders.push(roster);
     // add one to count to increment our recursive loop by one
-    // count++;
+    count++;
     // run the askquestion function again so as to
     // either end the loop or ask the questions again
-    askQuestion(answers.isDone);
+    addPlayer();
   });
 };
 
 // call askquestion to run our code
-askQuestion();
+addPlayer();
+// const fillstarters= function() {
+//   addPlayer(2, starters);
+// };
+//   const fillsubs= function() {
+//     addPlayer(1, subs);
+//   };
+//   fillstarters(2);
+//   fillsubs(1);
+// };
