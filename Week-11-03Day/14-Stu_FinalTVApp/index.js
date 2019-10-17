1b70d848c74314464e5194779204236b91cb51b9
Nick Frattaroli <Nafratt@gmail.com>
2019-10-17 18:13:14 -0400

    adding class stuff

--- Week-11-03Day/14-Stu_FinalTVApp/index.js
+++ Week-11-03Day/14-Stu_FinalTVApp/index.js
@@ -1,44 +1,58 @@
-// dependency for inquirer npm package
+/* eslint-disable require-jsdoc */
 const inquirer = require('inquirer');
-
-const Team = require('./models/Team');
-const Tournament = require('./models/Tournament');
-
-/**
- * This is the main entry point to the whole application
- */
-function playTeamManager() {
-  const team = new Team(2, 1);
-  team.buildTeamAsync()
-      .then(() => {
-        const tournament = new Tournament(team, 5);
-        return tournament.playTournamentAsync();
-      })
-      .then(() => {
-        return inquirer.prompt([
-          {
-            name: 'playAgain',
-            type: 'confirm',
-            message: `Do you want play again?`,
-          },
-        ])
-            .then((answers) => {
-              if (answers.playAgain) playTeamManager();
+const axios = require('axios');
+const fs = require('fs');
+inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Chose an action',
+        choices: ['Show', 'Actor'],
+        name: 'choice',
+      },
+    ])
+    .then(function(inquirerResponse) {
+      console.log(inquirerResponse.choice);
+      function getUserResponse(inquirerResponse) {
+        switch (inquirerResponse) {
+          case 'Show':
+            getShow();
+            break;
+          case 'Actor':
+            getActor();
+            break;
+        }
+      }
+      getUserResponse(inquirerResponse.choice);
+    });
+function getShow() {
+  inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'Enter the name of the show:',
+          name: 'showname',
+        },
+      ])
+      .then(function(inquirerResponse) {
+        console.log(inquirerResponse.showname);
+        const queryURL = 'http://api.tvmaze.com/singlesearch/shows?q=' + inquirerResponse.showname;
+        axios.get(queryURL).then(
+            function(showResponse) {
+              console.log('Show ' + showResponse.data.name);
+              console.log('Genre(s) ' + showResponse.data.genres);
+              console.log('Rating: ' + showResponse.data.rating.average);
+              console.log('Network: ' + showResponse.data.network.name);
+              console.log('Summary: ' + showResponse.data.summary);
+              fs.appendFile('show.txt', 'utf8',
+                  'Show ' + showResponse.data.name + 'Genre(s) ' +
+            showResponse.data.genres +
+            'Rating: ' + showResponse.data.rating.average +
+            'Network: ' + showResponse.data.network.name +
+            'Summary: ' + showResponse.data.summary,
+              );
             });
-      });
-}
-
-console.log(`
-ULTIMATE TEAM MANAGER - THE GAME
-================================
-`);
-
-playTeamManager();
-
-
-
-
-
-
-
+      }
+      );
+};
 
