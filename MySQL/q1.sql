e72bd38c3a30e31bd56899936fc9650112cd13d4
Nick Frattaroli <Nafratt@gmail.com>
2019-10-22 20:56:57 -0400

    adding all queries

--- MySQL/q1.sql
+++ MySQL/q1.sql
@@ -5,4 +5,18 @@ USE animals_db;
 INSERT INTO people (name, has_pet, pet_name, pet_age)
 VALUES ("Ahmed", true, "Rockington", 100);
 
+INSERT INTO people (name, has_pet)
+VALUES ("peter",false);
+
+UPDATE people
+SET has_pet = true
+WHERE name ="Peter" ;
+
+INSERT INTO people (name, has_pet, pet_name, pet_age)
+VALUES ('Rachael', true, 'Minnie', 2),
+('Rachael', true, 'Nipper', 2);
+
+
+DELETE FROM `animals_db`.`people`
+WHERE pet_name = 'rockington';
 
