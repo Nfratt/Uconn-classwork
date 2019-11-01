71ebd245eface0186a49594f607a35eb0d5e40df
Nick Frattaroli <Nafratt@gmail.com>
2019-10-31 20:13:56 -0400

    server solved

--- express-day-2/03-SeinfeldApp/server.js
+++ express-day-2/03-SeinfeldApp/server.js
@@ -13,3 +13,49 @@ const PORT = process.env.PORT || 8080;
 // Global variable that will hold DB connection
 let connection;
 
+// Routes
+app.get('/', async function(req, res) {
+  // If the main route is hit, then we initiate a SQL query to grab all records.
+  // All of the resulting records are stored in the variable "result."
+  const result = await connection.query('SELECT * FROM actors');
+
+  // We then begin building out HTML elements for the page.
+  let html = '<h1> SEINFLED </h1>';
+
+  // Here we begin an unordered list.
+  html += '<ul>';
+
+  // We then use the retrieved records
+  // from the database to populate our HTML file.
+  for (let i = 0; i < result.length; i++) {
+    html += '<li><p> ID: ' + result[i].id + '</p>';
+    html += '<p>name: ' + result[i].name + ' </p>';
+    html += '<p>coolness: ' + result[i].coolness_points + ' </p>';
+    html += '<p>attitude ' + result[i].attitude + ' </p></li>';
+  }
+
+  // We close our unordered list.
+  html += '</ul>';
+
+  // Finally we send the user the HTML file we dynamically created.
+  res.send(html);
+});
+// Start our server so that it can begin listening to client requests.
+// Once server has started up, set up the DB connection
+app.listen(PORT, async function() {
+  // Log (server-side) when our server has started
+  console.log('SERVER LISTENING ON: http://localhost:' + PORT);
+  console.log('----------------------------');
+
+  // Now that server is listening,
+  // create DB Connection
+  try {
+    connection = await mysql.createConnection(require('./db-config'));
+  } catch (error) {
+    console.log('ERROR: DB CONNECTION FAILED');
+    console.table(error);
+    process.exit(1);
+  }
+  console.log('DATABASE CONNECTION ESTABLISHED');
+  console.table(connection.config);
+});
