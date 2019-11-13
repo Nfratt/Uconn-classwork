514e24fb4e6a5fa805da3adff68ae324e4977fac
Nick Frattaroli <Nafratt@gmail.com>
2019-11-12 20:46:41 -0500

    server crashed

--- 11postmodel/11-Post-Model/models/post.js
+++ 11postmodel/11-Post-Model/models/post.js
@@ -21,6 +21,18 @@ module.exports = function(sequelize, DataTypes) {
       defaultValue: 'Personal',
     },
   });
+  Post.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Post.belongsTo(models.Author, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+
+
+
   return Post;
 };
 // this style allows you to use mutiple tabels
