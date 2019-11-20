b25a33b0bb8f998d964b34bf42f73170263d3a06
Nick Frattaroli <Nafratt@gmail.com>
2019-11-19 19:08:30 -0500

    working unit test

--- unittests/09-Stu_Check-For-Exceptions/Unsolved/assert.js
+++ unittests/09-Stu_Check-For-Exceptions/Unsolved/assert.js
@@ -2,7 +2,14 @@ var assertThrows = function(func, x, y) {
   var threw = false;
   // Wrap func with a try/catch
   // If an error is thrown, set threw to true
-  func(x, y);
+  try {
+    func(x, y);
+
+  } catch (error) {
+    threw=true;
+    console.log(error)
+  }
+ 
 
 
   // Depending on whether an error was thrown, threw is either true or false
@@ -12,8 +19,13 @@ var assertThrows = function(func, x, y) {
 var multiply = function(x, y) {
   // use 'typeof' keyword to check if both x and y are numbers:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
-  if ("** some check goes here **") {
+  if (typeof x !== "number"|| typeof y !== "number") {
+    var arr=['a',]
     // throw an error if either x or y is not a number
+    throw new Error('did not enter a number')
   }
   else return x * y;
 };
+
+console.log('for a & 3 assertThrows ', assertThrows(multiply,'a',3));
+console.log('for 5 & 3 assertThrows ', assertThrows(multiply,5,3));
