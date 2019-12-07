4a67d4b693c21de217d31a3dfeb5981954f1e81f
Nick Frattaroli <Nafratt@gmail.com>
2019-12-07 11:50:05 -0500

    updated user model

--- mongoose/15-User-Schema/userModel.js
+++ mongoose/15-User-Schema/userModel.js
@@ -11,6 +11,7 @@ const UserSchema = new Schema({
    *
 
    * 1: username: A string that will be be required, and also trimmed.
+  
    * 2: password: A string that will be required, trimmed, and at least 6 characters.
    * 3: email: A string that must be a valid email address and unique in our collection.
    * 4: userCreated: A date that will default to the current date.
@@ -18,6 +19,37 @@ const UserSchema = new Schema({
    * TIP: The regex for checking if a string is an email is: /.+\@.+\..+/
    * Use that with the model attribute that checks for a valid match.
    * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ */
+  username: {
+    type: String,
+    trim: true,
+    required: 'username is Required',
+  },
+  email: {
+    unique: true,
+    required: true,
+    type: String,
+    match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    validate: [
+      // Function takes in the new `longstring` value to be saved as an argument
+      function (input) {
+        // If this returns true, proceed. If not, return the error message below
+        return input.length >= 6;
+      },
+      // Error Message
+      'password should be longer.',
+    ],
+    userCreated: {
+      type: Date,
+      default: Date.now,
+    }
+
+  }
+
 });
 
 // This creates our model from the above schema, using mongoose's model method
