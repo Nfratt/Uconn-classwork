bb4f8f9b887fb0e1930a79b7f0ebbae839ecf182
Nick Frattaroli <Nafratt@gmail.com>
2019-12-10 21:12:23 -0500

    colins fix

--- ES6/08-Ins_FunctionalLoops/index.js
+++ ES6/08-Ins_FunctionalLoops/index.js
@@ -6,27 +6,31 @@ const moviePatrons = [
   { name: "Cherie", age: 14 },
   { name: "Malcolm", age: 15 }
 ];
-
+​
+const movieNames = [
+  'et',
+  'starwars'
+];
 // 1.
-
+​
 // forEach is a functional way of iterating through an array without a for-loop
 console.log('\n\n(1) forEach ==================================');
-
-moviePatrons.forEach(patron => console.log(patron.age));
-
+​
+const ret = moviePatrons.forEach(patron => console.log(patron.age));
+console.log(ret);
+​
 // 2.
-
+​
 // Filter returns a new array containing only elements whose callback returns a truthy value
-
+​
 console.log('\n\n(2) filter ==================================');
-const canWatchRatedR = moviePatrons.filter(function(patron) {
-  return patron.age > 17;
-});
-
+const canWatchRatedR = moviePatrons.filter((patron) => patron.age > 17);
+​
+console.log(moviePatrons);
 console.log(canWatchRatedR);
-
+​
 // 3.
-
+​
 // Map returns a brand new array the same length as the first. Each element is passed into the callback.
 // Whatever is returned from the callback at each iteration is what goes into that index of the new array
 console.log('\n\n(3) map ==================================');
@@ -38,6 +42,22 @@ const cardedMoviePatrons = moviePatrons.map(patron => {
   }
   return patron;
 });
-
+​
+const cardedMoviePatrons2 = moviePatrons.map(patron => {
+  patron.age >= 17 ? patron.canWatchRatedR = true : patron.canWatchRatedR = false;
+  return patron;
+});
+​
+const cardedMoviePatrons3 = moviePatrons.map(patron => {
+  patron.canWatchRatedR = patron.age >= 17;
+  return patron;
+});
+​
 console.log("Carded Movie Patrons: ");
-console.log(cardedMoviePatrons);
+console.log(cardedMoviePatrons2 );
+console.log(moviePatrons)
+​
+const newMovieNames = movieNames.map(name => name + ' hit!');
+console.log(newMovieNames);
+console.log(movieNames)
+​
